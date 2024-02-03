bb33546f236573216d3118014cfb981f2e7abd7a
yassingamalz <yassen.gamal.yg@gmail.com>
2024-02-03 20:38:34 +0100

    add live-price component

---
+++ src/app/live-price/live-price.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LivePriceComponent } from './live-price.component';
+
+describe('LivePriceComponent', () => {
+  let component: LivePriceComponent;
+  let fixture: ComponentFixture<LivePriceComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [LivePriceComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(LivePriceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/live-price/live-price.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Item {
+  name: string;
+  price: number;
+  change: number;
+}
+
+@Component({
+  selector: 'app-live-price',
+  templateUrl: './live-price.component.html',
+  styleUrls: ['./live-price.component.css']
+})
+export class LivePriceComponent implements OnInit {
+  items: Item[] = [];
+
+  ngOnInit() {
+    this.generateRandomItems(); // Generate random items and changes
+  }
+
+  generateRandomItems(): void {
+    const itemNames = ['لبن', 'لحم', 'دجاج', 'خبز', 'بطاطس', 'زيت', 'سكر', 'أرز', 'ملح', 'فول'];
+
+    for (let i = 0; i < 10; i++) { // Generate 10 random items and changes
+      const name = itemNames[Math.floor(Math.random() * itemNames.length)];
+      const price = parseFloat((Math.random() * 100 + 10).toFixed(2)); // Round to 2 decimal places
+      const change = Math.random() * 0.1 - 0.05; // Generate random change between -5% and 5%
+
+      this.items.push({ name, price, change });
+    }
+  }
+}
+
