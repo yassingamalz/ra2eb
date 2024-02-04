35827ec42ebc64e3acc20481c7c5a30aa8b18841
yassingamalz <yassen.gamal.yg@gmail.com>
2024-02-04 16:23:31 +0100

    create news component logic

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
-  styleUrl: './news.component.css'
+  styleUrls: ['./news.component.css'],
 })
-export class NewsComponent {
+export class NewsComponent implements OnInit {
+  articles: Article[] = [];
 
+  ngOnInit(): void {
+    this.generateRandomArticles();
+  }
+
+  private generateRandomArticles(): void {
+    for (let i = 1; i <= 5; i++) {
+      const article: Article = {
+        id: i,
+        title: `Article ${i}`,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        imageUrl: `https://placekitten.com/150/150?image=${i}`, // Replace with your actual image URL
+      };
+
+      this.articles.push(article);
+    }
+  }
+}
+
+export interface Article {
+  id: number;
+  title: string;
+  description: string;
+  imageUrl: string;
 }
